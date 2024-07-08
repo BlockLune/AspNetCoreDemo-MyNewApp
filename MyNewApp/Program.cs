7015d4cd9fa1df1def3725adaec7874141ed0663
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-07-08 11:31:40 +0800

    Pt 3 Completed

--- MyNewApp/Program.cs
+++ MyNewApp/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var todos = new List<Todo>();
+
+app.MapGet("/todos", () => todos); // Not use TypedResults<T> here. The minimal API will recognize it and serialize it to JSON automatically.
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+  var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+  return targetTodo is null
+    ? TypedResults.NotFound()
+    : TypedResults.Ok(targetTodo);
+});
+app.MapPost("/todos", (Todo task) =>
+{
+  todos.Add(task);
+  return TypedResults.Created("/todos/{id}", task);
+});
+app.MapDelete("/todos/{id}", (int id) =>
+{
+  todos.RemoveAll(t => t.Id == id);
+  return TypedResults.NoContent(); // http code 204
+});
 
 app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MyNewApp
 
 See [Back-end Web Development with .NET for Beginners](https://www.youtube.com/playlist?list=PLdo4fOcmZ0oWunQnm3WnZxJrseIw2zSAk)
+
+## Pt 3
+
+CRUD Patterns: Create, Read, Update, Delete
